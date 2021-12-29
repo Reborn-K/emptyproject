eed9e557a0cd9dbdc577fe834758d717f03ee152
oyk <1309145155@qq.com>
2021-12-29 19:11:34 +0800

    push commit

--- git-test/src/main/java/com/ou/git/GitTest.java
+++ git-test/src/main/java/com/ou/git/GitTest.java
@@ -10,6 +10,6 @@ public class GitTest {
         System.out.println("hello Git 3");
         System.out.println("hello Git 4");
         System.out.println("master");
-        System.out.println("fix");
+        System.out.println("push");
     }
 }
