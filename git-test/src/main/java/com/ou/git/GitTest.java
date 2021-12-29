92f8dd51ba178321573ad633b4bdd6632bafb451
Reborn-K <96785238+Reborn-K@users.noreply.github.com>
2021-12-29 19:22:33 +0800

    pull commit

--- git-test/src/main/java/com/ou/git/GitTest.java
+++ git-test/src/main/java/com/ou/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello Git 3");
         System.out.println("hello Git 4");
         System.out.println("master");
-        System.out.println("push");
+        System.out.println("push"); 
+        System.out.println("pull");
     }
 }
