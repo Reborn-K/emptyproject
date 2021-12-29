12705a5b6e832cfc12c0bcf4b0405afa958f4e91
oyk <1309145155@qq.com>
2021-12-29 17:30:55 +0800

    second commit

--- git-test/src/main/java/com/ou/git/GitTest.java
+++ git-test/src/main/java/com/ou/git/GitTest.java
@@ -2,10 +2,10 @@ package com.ou.git;
 
 /**
  * @author OYK
- * @createTime 2021-12-29 17:09
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello Git 2");
     }
 }
